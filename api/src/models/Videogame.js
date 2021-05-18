261d895ec8cb2d3f95979fca4eec021125ca1bc0
Sebastian <tassone_af@yahoo.com>
2021-05-18 10:36:20 -0300

    database ready

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,41 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+
+    releaseDate: {
+      type: DataTypes.STRING
+    },
+
+    raiting: {
+      type: DataTypes.DECIMAL
+    },
+
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   });
 };
+
