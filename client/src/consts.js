9e80462cbdf9ad6c12a6e36d272f640545a50f52
Sebastian <tassone_af@yahoo.com>
2021-05-21 03:57:10 -0300

    const

---
+++ client/src/consts.js
@@ -0,0 +1,3 @@
+export const IMG_EMPTY = 'https://lh3.googleusercontent.com/proxy/c_J94Uf1pSHKAyE82d9IaU6o2kDdZ8eIlgCnCvDGNyNwLHLeA2908wStKdLSktZDdrSG22zvMg179DmQYD58SIcFW-8i2b2xdfZ7QB1uPpZh';
+export const IMG_DEFAULT = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTonngp18Bf1FItfyqC_nsy914JiXymgUFvNYydZNatuYxGikb4ugtXOBg0JAgJaSOlaQ4&usqp=CAU';
+export const IMG_ADDITIONAL = 'https://fondosmil.com/fondo/3617.png';
