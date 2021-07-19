3116b28e316078cc71862387ef81dbb622f50752
Sebastian <tassone_af@yahoo.com>
2021-07-19 01:53:38 -0300

    Add: about me component

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import img from '../../imgs/problemas_tecnicos.jpg';
+import git from './img/git.png';
+import inL from './img/in2.png';
+import './About.css';
 
 export default function About() {
 
-    return (
-        <div>
-            <br /> <br /><br /> <br /><br /> <br />
-            <Link to='/home'>
-                <img  src={img} alt='' />
-            </Link>
-
+  return (
+    <div className="about">
+        <h2>By Sebastián E. Tassone</h2>
+        <h3>Contact me:</h3>
+        <div className="AboutImgs">
+          <a href="https://www.linkedin.com/in/tassone-seb/" target="_blank" rel="noreferrer">
+            <img className="fadein" src={inL} alt="" />
+          </a>
+          <a href="https://github.com/sbs001" target="_blank" rel="noreferrer">
+            <img className="fadein" src={git} alt="" />
+          </a>
         </div>
-    )
-
-};
+    </div>
+  )
+}
